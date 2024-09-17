a1c2b5575ed545a06a7de0df153efc77593b4990
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-09-17 13:04:17 +0530

    setup hono js for api routing

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,66 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import authors from './authors'
+import books from './books'
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+// app
+// .get('/hello',  (c) => {
+//   return c.json({
+//     message: 'Hello Next.js!',
+//   })
+// })
+// .get("/hello/:test", 
+//     zValidator("param", z.object({
+//         test: z.string()
+//     })),
+    
+//     (c) => {
+//     const {test} = c.req.valid("param");
+//     return c.json({
+//         message: "hello world",
+//         test: test
+//     })
+// })
+// .post("/create/:postId", 
+//   zValidator("json", z.object({
+//     name: z.string(),
+//     userId: z.string()
+//   })),
+//   zValidator("param", z.object({
+    
+//     postId: z.string()
+//   })),
+//   (c) => {
+//   const {postId} = c.req.valid("param")
+//   const {name, userId} = c.req.valid("json")
+//   return c.json({
+
+//   })
+// })
+
+app.route('/authors', authors)
+app.route('/books', books)
+
+app
+.get('/hello', clerkMiddleware(),  (c) => {
+  const auth = getAuth(c)
+  if(!auth?.userId){
+    return c.json({
+      error: "Unauthorized"
+    })
+  }
+  return c.json({
+    message: 'Hello Next.js!',
+    userId: auth.userId
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
